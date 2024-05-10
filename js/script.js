215c8676f136bf333abd5a616c35c156fa982bcd
Ivan Evans <74743263+Pasithea0@users.noreply.github.com>
2024-05-09 23:48:33 -0600

    updates

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
-function handleDeviceMotion(event) {
-  console.log('handleDeviceMotion event:', event);
+  function handleDeviceMotion(event) {
+    const { alpha, beta } = event.rotationRate;
+    const scaleFactor = 5;
 
-  if (event.rotationRate) {
-    const { alpha, beta, gamma } = event.rotationRate;
-    const scaleFactor = 10;
-
-    document.querySelector('.logo').style.transform = `rotateX(${alpha * scaleFactor}deg) rotateY(${beta * scaleFactor}deg) rotateZ(${gamma * scaleFactor}deg)`;
-  } else {
-    console.error('No rotationRate data in event.');
+    document.querySelector('.logo').style.transform = `skewX(${alpha * scaleFactor}deg) skewY(${beta * scaleFactor}deg)`;
   }
-}
 
-if ('DeviceMotionEvent' in window) {
-  window.addEventListener('devicemotion', handleDeviceMotion);
-  console.log('DeviceMotionEvent is supported, listening for events...');
-} else {
-  console.error('DeviceMotionEvent is not supported in this browser.');
-}
+  if ('DeviceMotionEvent' in window) {
+    window.addEventListener('devicemotion', handleDeviceMotion);
+  } else {
+    console.error('DeviceMotionEvent is not supported in this browser.');
+  }
